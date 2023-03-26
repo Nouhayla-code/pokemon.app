4e7f6aceba90cbda1eac8977d2d32417d28655b3
Nouhayla-code <nouhayla@hotmail.dk>
2023-03-27 00:07:20 +0200

    json

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+window.addEventListener("load", initApp);
+
+const Quagsire = {
+  pokeName: "Quagsire",
+  description:
+    "It has an easygoing nature. It doesnt care if it bumps its head on boats and boulders while swimming",
+  abilityOne:
+    "DAMP:The Pokémon dampens its surroundings, preventing all Pokémon from using explosive moves such as Self-Destruct.",
+  abilityTwo:
+    "WATER ABSORB:If hit by a Water-type move, the Pokémon has its HP restored instead of taking damage.",
+  number: 195,
+  height: 139.7,
+  weight: 73935.5,
+  weaknesses: "Grass",
+  gender: "male and female",
+  canEvolve: true,
+  image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/195.png",
+};
+
+
+// footprint: url (til et andet billede) 
+// type: tekst – begrænset til: fire, ice, flying, etc …
+// subtype: tekst
+
+
+
+// generation: tal
+// spilversion: tekst
+
+
+
+function initApp(){
+}
+function fetchJSON(){
+
+}
+function showPokemons(){
+
+}
+function showQuagsire(Quagsire){
+  document.querySelector("body").insertAdjacentHTML("afterbegin",
+  
+  
+  );
+}
