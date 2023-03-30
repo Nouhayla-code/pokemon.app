2571c29e7cc983513f013b19a973dd98a428b96a
Nouhayla-code <nouhayla@hotmail.dk>
2023-03-30 09:17:15 +0200

    små ændringer

--- script.js
+++ script.js
@@ -2,74 +2,82 @@
 
 window.addEventListener("load", initApp);
 
-async function initApp(){
-  const pokemons=await getPokemons("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
-  showPokemons();
-}
-
-
-
-function initApp(){
-}
-function fetchJSON(){
-
-}
-function showPokemons(){
-
-}
-function showQuagsire(Quagsire){
-  document.querySelector("body").insertAdjacentHTML("afterbegin",
-  
-  
+async function initApp() {
+  const pokemonData = await fetchJSON(
+    "https://cederdorff.github.io/dat-js/05-data/pokemons.json"
   );
-}
-  document.querySelector("#pokemon-list").insertAdjacentHTML(
-    "beforeend",
-    /*html*/ `
-
-<article class="list-entry">
-    <img id="list-image" src = "${pokemon.image}"/>
-    <div class="type-color-${typeColor}"></div>
-    <h2 id="list-name">${pokemon.name}</h2>
-    <h3 id="list-number">#${dexNumber}</h3>
-    <p id="list-description">${pokemonDesc}</p>
-</article>
-`
-  );
-
-  document.querySelector("#pokemon-list article:last-child").addEventListener("click", pokemonClicked);
-
-  function pokemonClicked() {
-    document.querySelector("#background").classList.remove("dim");
-    document.querySelector("#background").classList.add("dark");
-    document.querySelector("#pokemon-list").classList.add("dark");
-    document.querySelector("#dialog-color").classList.add(`dialog-type-${typeColor}`);
-
-    document.querySelector("#pokemon-name").textContent = pokemon.name;
-    document.querySelector("#pokemon-type").textContent = `Type: ${pokemon.type}`;
-    document.querySelector("#pokemon-dex").textContent = `Dex Number: ${dexNumber}`;
-    document.querySelector("#pokemon-image").src = pokemon.image;
-    document.querySelector("#pokemon-footprint").src = pokemon.footprint;
-    document.querySelector("#pokemon-description").textContent = pokemon.description;
-    document.querySelector(".pokemon-hp").textContent = `HP: ${pokemon.statsHP}`;
-    document.querySelector(".pokemon-attack").textContent = `ATTACK: ${pokemon.statsAttack}`;
-    document.querySelector(".pokemon-defense").textContent = `DEFENSE: ${pokemon.statsDefence}`;
-    document.querySelector(".pokemon-spattack").textContent = `SP.ATTACK: ${pokemon.statsSpecialAttack}`;
-    document.querySelector(".pokemon-spdefense").textContent = `SP.DEFENSE: ${pokemon.statsSpecialDefence}`;
-    document.querySelector(".pokemon-speed").textContent = `SPEED: ${pokemon.statsSpeed}`;
-    document.querySelector("dialog").showModal();
-    document.querySelector(".btn-close").addEventListener("click", pokemonClose);
+  for (const pokemon of pokemonData) {
+    showPokemon(pokemon);
   }
 }
 
-function pokemonClose() {
-  document.querySelector("dialog").close();
-  document.querySelector("#background").classList.remove("dark");
-  document.querySelector("#pokemon-list").classList.remove("dark");
-  document.querySelector("#dialog-color").removeAttribute("class");
-  document.querySelector("#background").classList.add("dim");
+async function fetchJSON(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-function removePokemon() {
-  document.querySelectorAll(".list-entry").forEach((e) => e.remove());
+function showPokemon(pokemon) {
+  console.log("showpokemon");
+  const pokemonHTML = /*html*/ `
+    <article class="grid-item">
+        <img src="${pokemon.image}">
+        <h2>${pokemon.name}</h2>
+        <p>${pokemon.type}</p>
+    </article>`;
+  document
+    .querySelector("#pokemon")
+    .insertAdjacentHTML("beforeend", pokemonHTML);
+  document
+    .querySelector("#pokemon article:last-child")
+    .addEventListener("click", function () {
+      clickPokemon(pokemon);
+    });
+}
+
+function clickPokemon(pokemon) {
+  const id = `pokemon-info-${pokemon.name}`;
+  const detailHTML = /*html*/ `
+    <article id="${id}">
+      <h1>${pokemon.name}</h1>
+      <img class=pokemon-info-img src="${pokemon.image}">
+      <h3>"${pokemon.description}"</h3>
+      <li>Footprint: <img class="footprint"src="${pokemon.footprint}"></li>
+      <li>Ability: ${pokemon.ability}</li>
+      <li>Type: ${pokemon.type}</li>
+      <li>Subtype: ${pokemon.subtype}</li>
+      <li>Weaknesses: ${pokemon.weaknesses}</li>
+      <li>Gender: ${pokemon.gender}</li>
+      <li>Weight: ${pokemon.weight} grams</li>
+      <li>Height: ${pokemon.height} centimeters</li>
+      <li>Generation: ${pokemon.generation}</li>
+      <li>Game version: ${pokemon.spilversion}</li>
+      <li>Can it evolve: ${pokemon.canEvolve}</li>
+      <li>HP: ${pokemon.statsHP}</li>
+    </article>`;
+  document
+    .querySelector("#pokemon-details")
+    .insertAdjacentHTML("beforeend", detailHTML);
+
+  document.querySelector("#pokemon-details").showModal();
+
+  document.querySelector("#close-btn").addEventListener("click", closeDialog);
+}
+
+function closeDialog() {
+  console.log("closeDialog");
+  document.querySelector("#pokemon-details").close();
+  document.querySelector("#pokemon-info-" + pokemon.name).remove();
+}
+
+function showGeneration() {
+  // implementation goes here
+}
+
+function showTyping() {
+  // implementation goes here
+}
+
+function getDescription() {
+  // implementation goes here
 }
